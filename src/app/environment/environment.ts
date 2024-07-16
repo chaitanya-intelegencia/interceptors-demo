0538a89d818c9b2ed54b777118e0ccc15b3ad90d
chaitanya-intelegencia <chaitanya@intelegencia.com>
2024-07-16 18:24:34 +0530

    env add

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,25 +1,5 @@
 // src/environments/environment.ts
-// export const environment = {
-//     production: false,
-//     apiUrl: 'http://172.16.2.212:1001'
-//   };
-
-
-  export const environment = [
-    {
-        "name": "North_Sea",
-        "IP_ADDRESS": "localhost",
-        "Active": "false",
-        "Type": "Plant",
-        "Is_Authenticated": "",
-        "PORT": "5696"
-    },
-    {
-        "name": "Paciifc_location",
-        "IP_ADDRESS": "localhost",
-        "Active": "true",
-        "Type": "High_Availability",
-        "Is_Authenticated": "",
-        "PORT": "8080"
-    }
-]
+export const environment = {
+    production: false,
+    apiUrl: 'http://172.16.2.212:1001'
+  };
